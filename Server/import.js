e402db7950df838ceade9f92e270f986214beebe
Anh Lâm <lamducanhndgv@gmail.com>
2020-06-12 06:32:18 +0700

    track new files

---
+++ Server/import.js
@@ -0,0 +1,16 @@
+// Require the express module
+const express = require('express');
+
+// Create a new express application
+const app = express();
+
+// Create server 
+const http = require("http").createServer(app);
+
+// import Socket.IO
+const io = require('socket.io')(http);
+
+module.exports.expr = express;
+module.exports.application = app;
+module.exports.http = http;
+module.exports.IO = io;
